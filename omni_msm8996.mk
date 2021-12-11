a79c1dcff2805f36f28c445e5a4749123f41d75b
zeng-github01 <95841646+zeng-github01@users.noreply.github.com>
2021-12-11 17:23:27 +0800

    Update omni_msm8996.mk

--- omni_msm8996.mk
+++ omni_msm8996.mk
@@ -24,6 +24,4 @@ PRODUCT_BRAND := Android
 PRODUCT_MODEL := Readboy_C20
 PRODUCT_MANUFACTURER := readboy
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=readboy/msm8996/msm8996:7.1.2/N2G47H/20200911:user/dev-keys \
-    ro.product.device=msm8996
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="readboy/msm8996/msm8996:7.1.2/N2G47H/20200911:user/dev-keys"
